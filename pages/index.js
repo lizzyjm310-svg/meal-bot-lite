0db9d173c17d6d0b93f3e9691c49dd1bf0f986ca
lizzyjm310-svg <lizzyjm310@gmail.com>
2025-08-24 14:19:11 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: "sans-serif", padding: "2rem" }}>
+      <h1>Meal Bot Lite</h1>
+      <p>Welcome! This is your Meal Planning Bot.</p>
+      <p>Type your questions in the chat once we hook up the UI.</p>
+    </div>
+  );
+}
